9dac96e2e7970abdb9073dd0467d791d6f2d3157
“Sixiang <z5280561@unsw.edu.au>
2020-05-24 16:54:37 +1000

    1

--- comp9315/ass1/myTest.sql
+++
@@ -1,8 +0,0 @@
-create table Students (
-   zid       integer primary key,
-   name      PersonName not null
-);
-
-insert into Students(zid,name) values
-(9300035,'Shepherd, John Andrew'),
-(5012345,'Smith, Stephen');
